521acb2370d6ce082c023343dc280b653fece547
陈真 <361914037@163.com>
2016-12-01 04:18:42 +0800

    add server side functional: 1.install modules 'koa-body','koa-bodyparser','koa-router' 2.config server side request path

--- server/index.js
+++ server/index.js
@@ -1,20 +1,58 @@
 import koa from 'koa';
 
+
+var router = require('koa-router')({
+    prefix:'/api'
+});
+
+var bodyParser = require('koa-bodyparser');
+
 var koaServer=new koa();
 
-koaServer.use(async (ctx,next)=>{
-    try{
-        next();
-    }
-    catch(err){
-        ctx.body='server inside faild';
-        ctx.status=err.status||500;
-    }
+var users=[
+    "Bob",
+    "Tom",
+    "Richard"
+];
+
+router
+.get('/',async(ctx,next)=>{
+    ctx.body="home page";
 })
 
-koaServer.use(async (ctx)=>{
-    ctx.body='hello guy';
+.param('user',async(id,ctx,next)=>{
+    ctx.user=users[id];
+    console.info(id)
+    if (!ctx.user) return ctx.status = 404;
+    next();
+})
+.get('/first/:user',async(ctx,next)=>{
+    ctx.body=`
+        <html>
+            <head>
+                <script src="http://lib.sinaapp.com/js/jquery/1.9.1/jquery-1.9.1.min.js"></script>
+            </head>
+            <body>
+                <div style="background:red;">${ctx.user}</div>
+            </body>
+        </html>
+    `;
 });
+
+var postRouter=new require('koa-router')({
+    prefix:'/api'
+});
+
+postRouter.post('/second',async(ctx,next)=>{
+   var {user}=ctx.request.body;
+    ctx.body=`${users[user]}`;
+})
+
+koaServer
+.use(bodyParser())
+.use(router.routes())
+.use(postRouter.routes())
+.use(router.allowedMethods())
 
 koaServer.listen(3000);
 console.info('server is launch,and listen port 3000.');
