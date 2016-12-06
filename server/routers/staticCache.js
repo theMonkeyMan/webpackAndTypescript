ccf780565e8cd382644c4e0e5f1c812d16c39ab3
陈真 <361914037@163.com>
2016-12-06 23:54:06 +0800

    validate request params: 1.validate ctx.request.get('If-Modified-Since') is empty.

--- server/routers/staticCache.js
+++ server/routers/staticCache.js
@@ -1,5 +1,5 @@
 export async function isStaticAssetsCache (ctx,next){
-    if (ctx.request.get('If-Modified-Since') == ctx.response.get('Last-Modified')) {
+    if (ctx.request.get('If-Modified-Since')&&ctx.request.get('If-Modified-Since') == ctx.response.get('Last-Modified')) {
             ctx.status = 304;
         }
         await next();
