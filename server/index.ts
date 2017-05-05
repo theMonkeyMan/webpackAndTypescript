08b0260be706d154a22776d22be79a8842b34154
RichardChen <cz@sctek.com>
2017-05-05 16:34:05 +0800

    develeoping points: 1.add modules 'koa-session','koa-mysql-session' 2.edit initDB cli in the package.json 3.use 'koa-session','koa-mysql-session' in the index.ts

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,10 @@ import * as koa from 'koa';
 
 import * as koaCompress from 'koa-compress';
 
+import * as session from 'koa-session';
+
+import * as mysqlStore from 'koa-mysql-session';
+
 var exec=require('child_process').exec;
 
 var os=require('os');
@@ -16,6 +20,18 @@ import { filterRouter, router, originChecker, isStaticAssetsCache } from './rout
 //创建koa实例
 var koaServer = new koa();
 
+var mysqlConfig={
+    host:'127.0.0.1',
+    user:'root',
+    password:'',
+    port:3306,
+    database:'myHome'
+};
+
+var store=new mysqlStore(mysqlConfig);
+
+koaServer.keys=['myHome'];
+
 koaServer
     //校验合法域名
     .use(originChecker)
@@ -25,6 +41,7 @@ koaServer
     .use(filterRouter)
 
     .use(isStaticAssetsCache)
+    .use(session({store:store},koaServer))
     /**
     * routes代表router对象配置的所有路径match规则集合,类型为Array,每一个请求都会遍历routes()集合里面的路由规则,
     * match到后执行相应的函数.for example:
@@ -40,13 +57,24 @@ koaServer
     //允许接收所有的请求方式,for example:get post put delete等.
     .use(router.allowedMethods())
     .use(koaCompress())
+    .use(async(ctx,next)=>{
+        if(!ctx['session'].view){
+            ctx['session'].view=0;
+        }
+        logger(ctx['session'].view++);
+        // ctx['session'].age=24;
+    })
 koaServer.listen(3000).addListener('listening',function(){
-    var osPlatform=os.platform();
-    if(osPlatform.includes("win32")||osPlatform.includes("win64")){
-        exec(`start http://127.0.0.1:3000`);
-    }
-    else{
-        exec(`open http://127.0.0.1:3000`);
-    }
+    // var osPlatform=os.platform();
+    // if(osPlatform.includes("win32")||osPlatform.includes("win64")){
+    //     exec(`start http://127.0.0.1:3000`);
+    // }
+    // else{
+    //     exec(`open http://127.0.0.1:3000`);
+    // }
 })
 console.info('server is launch,and listen port 3000.');
+
+function logger(data){
+    console.info(`path: ${__filename}:\n`, data);
+}
